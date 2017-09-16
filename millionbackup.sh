fc5c9623e2478f6a4d50468daf14b8372dcd6a1b
bommenamadhavi <31123170+bommenamadhavi@users.noreply.github.com>
2017-09-16 10:56:59 +0530

    Update millionbackup.sh

--- millionbackup.sh
+++ millionbackup.sh
@@ -4,8 +4,8 @@ mkdir -p /home/centos/backup/workdir
 time_stamp=$(date +%Y_%m_%d_%H_%M_%S)
 mkdir -p "/home/centos/backup/workdir/${time_stamp}"
 
-sudo  mysqldump --routines -u root -pTest123 Demo> "/home/centos/backup/workdir/${time_stamp}/Demo.sql"
-sudo  mysqldump --routines -u root -pTest123 $1 > "/home/centos/backup/workdir/${time_stamp}/$1.sql"
+sudo  mysqldump --routines -u root -pMillion@123 blogapp> "/home/centos/backup/workdir/${time_stamp}/blogapp.sql"
+sudo  mysqldump --routines -u root -pMillion@123 $1 > "/home/centos/backup/workdir/${time_stamp}/$1.sql"
 cd "/home/centos/backup/workdir/${time_stamp}/"
 tar cvfz "$1_${time_stamp}.tgz" *
 s3cmd put "$1_${time_stamp}.tgz"  s3://millionbackup
